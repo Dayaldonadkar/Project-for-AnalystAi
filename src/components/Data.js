c81682bd3843d8748863940b8a86ed03c6012e6a
Dayaldonadkar <Dayalpython@gmail.com>
2023-01-26 20:46:13 +0530

    first

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Datapi from "./Datapi";
 import Maps from "./Maps";
 
 const Data = () => {
-  const [detail, setdetails] = useState(Datapi);
+  const [detail, setdetails] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    setdetails(await response.json());
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="">
       <section className="bg-[#f4f7fc] h-max">
--- src/components/Maps.js
+++ src/components/Maps.js
@@ -8,22 +8,22 @@ const Maps = (currElem) => {
       <div className="w-4/5 flex flex-col">
         <div className="flex justify-between  items-center my-8 py-12  bg-white rounded-xl">
           <div>
-            <h1>{currElem.Company}</h1>
+            <h1>{currElem.company.name}</h1>
           </div>
 
           <div className="space-y-2">
             <p className="font-semibold">Contact</p>
-            <p>{currElem.Contact}</p>
+            <p>{currElem.name}</p>
           </div>
 
           <div className=" space-y-2">
             <p className="font-semibold">City</p>
-            <p>{currElem.City}</p>
+            <p>{currElem.address.street}</p>
           </div>
 
           <div className=" space-y-2">
             <p className="font-medium">State</p>
-            <p>{currElem.State}</p>
+            <p>{currElem.address.city}</p>
           </div>
 
           <div>
@@ -42,7 +42,9 @@ const Maps = (currElem) => {
                 <div className="w-4/5 space-y-4 border border-1 px-10 py-6 my-10 rounded-2xl shadow-xl">
                   <div className="space-y-3">
                     <h1 className="text-lg font-medium ">Description</h1>
-                    <p>{currElem.Details}</p>
+                    <p>{currElem.company.name}</p>
+                    <p>{currElem.company.catchPhrase}</p>
+                    <p>{currElem.company.bs}</p>
                   </div>
                   <div className="flex space-x-72">
                     <div className="space-y-3">
@@ -52,23 +54,23 @@ const Maps = (currElem) => {
                         </div>
                       </div>
                       <div>
-                        <p>{currElem.Contact}</p>
+                        <p>{currElem.name}</p>
                         <h1>Designation</h1>
                       </div>
 
                       <div>
-                        <p>{currElem.Designation}</p>
+                        <p>{currElem.username}</p>
                         <h1>Email</h1>
                         <h1></h1>
                       </div>
 
                       <div>
-                        {currElem.Email}
+                        {currElem.email}
                         <h1>Phone</h1>
                       </div>
 
                       <div>
-                        <p>{currElem.Phone}</p>
+                        <p>{currElem.phone}</p>
                       </div>
                     </div>
 
@@ -78,22 +80,24 @@ const Maps = (currElem) => {
                       </div>
 
                       <div>
-                        <p>{currElem.Address}</p>
+                        <p>
+                          {currElem.address.suite} {currElem.address.zipcode}
+                        </p>
                         <h1>City</h1>
                       </div>
 
                       <div>
-                        <p>{currElem.City}</p>
+                        <p> {currElem.address.street}</p>
                         <h1>State</h1>
                       </div>
 
                       <div>
-                        <p>{currElem.State}</p>
+                        <p>{currElem.address.city}</p>
                         <h1>Country</h1>
                       </div>
 
                       <div>
-                        <p>{currElem.Country}</p>
+                        <p>{currElem.address.city}</p>
                       </div>
                     </div>
                   </div>
